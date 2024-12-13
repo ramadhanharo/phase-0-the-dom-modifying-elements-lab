f8b48191d6ef93e60090a4a03a596259b2bdf6b3
ramadhanharo <ramadhan.haro@moringaschool.com>
2024-12-13 07:56:46 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here!
+const testVar = "hi"
+document.querySelector("main#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Ramadhan Haro is the champion!";
+newHeader.className = "victory";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
